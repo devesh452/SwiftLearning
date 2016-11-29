825b889d704f0747b7bcf5ea20fcfcc3eea33ddc
Devesh <devesh@macs-mac-mini.local>
2016-11-29 23:39:25 +0530

    Initial Commit

---
+++ SwiftLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLearning
+//
+//  Created by Devesh on 29/11/16.
+//  Copyright © 2016 __SELF___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
